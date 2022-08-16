a3ceedd45849b02c8f948c30f0365492cd89ae6e
mrgray <mister_gray@list.ru>
2022-08-16 17:50:28 +0300

    delete duplicete directory

--- lesson-1/task_1.py
+++
@@ -1,9 +0,0 @@
-print("П1")
-a = 10
-b = "hello"
-print(a)
-print(b)
-name = input("Введите ваше имя: ")
-age = input("Введите ваш возраст: ")
-print("Ваше имя: ", name)
-print("Ваш возоаст: ", age)
--- lesson-1/task_4.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Введите число: "))
-m = 0
-for n in str(num):
-    if int(n) > m:
-        m = int(n)
-print("Наибольшая из введенных цифр: ", m)
