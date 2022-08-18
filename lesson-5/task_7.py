d503d25dd5df3c78ed2b6d324989571cc51fea6d
mrgray <mister_gray@list.ru>
2022-08-18 11:54:19 +0300

    task_6.py resolve

--- lesson-5/task_7.py
+++ lesson-5/task_7.py
@@ -1,8 +1,18 @@
 #Создать вручную и заполнить несколькими строками текстовый файл, в котором каждая
 #строка будет содержать данные о фирме: название, форма собственности, выручка, издержки.
+
 import sys
 import json
-def firm_calc()
+
+firm_dict = {}
+profit_dict = {}
+fields = ['firm_name', 'firm_profit']
+res_profit = 0
+l = 1
+with open("firms.txt", encoding='utf-8') as f_firm:
+        for line in f_firm:
+            description = list(line.strip().split(None, 3))
+            print(description)
+            sno = 'firm_' + str(l)
 
 
-firm_calc()
