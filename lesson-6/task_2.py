0ba7263e376cf15710c828be90c5d1a85a4c7e64
mrgray <mister_gray@list.ru>
2022-08-18 22:19:37 +0300

    task_2.py resolve

--- lesson-6/task_2.py
+++ lesson-6/task_2.py
@@ -4,8 +4,8 @@ class Road:
     def calc(self):
         s = input("Введите массу асфальта для покрытия одного мерта (кг): ")
         t = input("Введите толщину покрытия (см): ")
-        bitum_calc = Road._length * Road._width * int(s) * int(t)/100
-        print("Результат (кг): ", bitum_calc)
+        bitum_calc = Road._length * Road._width * int(s) * int(t)
+        print("Результат (т): ", bitum_calc/1000)
 r = Road()
 r.calc()
 
