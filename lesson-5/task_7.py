c363271a4e08dded156d8e35cb63881ed6cc9f47
mrgray <mister_gray@list.ru>
2022-08-18 12:08:00 +0300

    task_7.py tmp commit

--- lesson-5/task_7.py
+++ lesson-5/task_7.py
@@ -9,10 +9,23 @@ profit_dict = {}
 fields = ['firm_name', 'firm_profit']
 res_profit = 0
 l = 1
+tmp_firm_desc = []
 with open("firms.txt", encoding='utf-8') as f_firm:
         for line in f_firm:
             description = list(line.strip().split(None, 3))
-            print(description)
+            tmp_firm_desc.append(description[0])
+            tmp_firm_desc.append(int(description[-2]) - int(description[-1]))
+            print(tmp_firm_desc)
             sno = 'firm_' + str(l)
-
+            i = 0
+            dict2 = {}
+            while i < len(fields):
+                dict2[fields[i]] = str(tmp_firm_desc[i][0])
+                i = i + 1
+            firm_dict[sno] = dict2
+            l = l + 1
+        out_file = open("firms.json", "w")
+        json.dump(firm_dict, out_file, indent=3)
+        out_file.close()
+        f_firm.close()
 
