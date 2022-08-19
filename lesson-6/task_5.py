b867e2b4d94d80bc0ab1073626e53b6f615ff0b7
mrgray <mister_gray@list.ru>
2022-08-19 10:13:21 +0300

    task_5.py resolve

--- lesson-6/task_5.py
+++ lesson-6/task_5.py
@@ -4,17 +4,14 @@ class Stationery:
         print("To draw not")
 
 class Pen(Stationery):
-    title_pen = "pen"
-    def draw_pen(self, title_pen):
-        print(f"TO draw: {title_pen}")
+        def draw_pen(self, title_pen):
+            print(f"TO draw: {title_pen}")
 
 class Pencil(Pen):
-    title_pensil = "pencil"
     def draw_pensil(self, title_pensil):
-        print(f"TO draw: {title_pensil}")
+            print(f"TO draw: {title_pensil}")
 
 class Handle(Pencil):
-    title_handle = "handle"
     def draw_handle(self, title_handle):
         print(f"TO draw: {title_handle}")
 
