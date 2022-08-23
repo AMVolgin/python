d175c8e0f3baadd9d68ac0ef0d49ed9bc23a38c3
mrgray <mister_gray@list.ru>
2022-08-23 11:10:05 +0300

    lesson 7 task 2 resolve

--- lesson-7/task_2.py
+++ lesson-7/task_2.py
@@ -8,7 +8,8 @@ class Coat(Clothes):
 
     def sizeCloth(self, V):
         size_cloth = round(((int(V) / 6.5) + 0.5), 2)
-        return f"Для пальто нужно {size_cloth} материала"
+        print(f"Для пальто нужно {size_cloth} материала")
+        return  size_cloth
 
 class Suit(Clothes):
     def __init__(self, H):
@@ -16,19 +17,22 @@ class Suit(Clothes):
         self.H = ""
 
     def sizeSuit(self, H):
-        size_suit = round((int(2*H) + 0.3), 2)
-        return f"Для пальто нужно {size_suit} материала"
+        size_suit = round((2 * int(H) + 0.3), 2)
+        print(f"Для пальто нужно {size_suit} материала")
+        return size_suit
 
-class MySummary(Coat,Suit):
-    def __init__(self):
-
-
-
+class allCloth(Coat, Suit):
+    def summaryCloth(self, ssize_suit, ssize_cloth):
+        summary = round(int(ssize_suit) + int(ssize_cloth), 2)
+        print(f"Ткани на одежду: {summary}")
 
 cc = Coat("V")
 tmp_v = input(f"Введите рост для пальто: {cc.V}")
-cc.sizeCloth(tmp_v)
+tmp_cloth = cc.sizeCloth(tmp_v)
 ss = Suit("H")
 tmp_h = input(f"Введите размер для костюма: {ss.H}")
-cc.sizeCloth(tmp_h)
+tmp_suit = ss.sizeSuit(tmp_h)
+ac = allCloth("ALL CLOTH")
+ac.summaryCloth(tmp_suit, tmp_cloth)
+
 
