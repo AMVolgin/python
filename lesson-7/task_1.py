a6bd520b5e207654b0974cb04fd6c83a8344b920
mrgray <mister_gray@list.ru>
2022-08-28 17:39:37 +0300

    lesson 7 устранение замечаний по task_1.py

--- lesson-7/task_1.py
+++ lesson-7/task_1.py
@@ -1,27 +1,23 @@
 class Matrix:
     def __init__(self, my_matrix1):
-        self.my_matrix1 = [[1, 3, 5], [2, 4, 6], [3, 7, 9]]
-        self.my_matrix2 = [[11, 13, 15], [12, 14, 16], [13, 17, 19]]
+        pass
 
 
-    def printMatrix(self, my_matrix1):
+    def __str__(self, my_matrix1):
+        print(f"{my_matrix1}")
+
+    def show_matrix(self, my_matrix1):
         for i in range(len(my_matrix1)):
             for j in range(len(my_matrix1[i])):
                 print("{:3d}".format(my_matrix1[i][j]), end="")
-            print()
-
-    def __str__(self, my_matrix1):
-            pass
 
     def __add__(self, my_matrix1, my_matrix2):
-            pass
 
-    def resultMatrix(self, my_matrix1, my_matrix2):
         i = 0
         j = 0
         x = 0
         res_matrix = []
-        res_matrix= [[my_matrix1[i][j] + my_matrix2[i][j] for j in range
+        res_matrix = [[my_matrix1[i][j] + my_matrix2[i][j] for j in range
         (len(my_matrix1[0]))] for i in range(len(my_matrix1))]
 
         for r in res_matrix:
@@ -29,9 +25,13 @@ class Matrix:
 
 
 mm = Matrix("MY_MATRIX")
-mm.printMatrix(mm.my_matrix1)
-print(mm.my_matrix1)
-mm.resultMatrix(mm.my_matrix1, mm.my_matrix2)
+mm1 = [[1, 3, 5], [2, 4, 6], [3, 7, 9]]
+mm2 = [[11, 13, 15], [12, 14, 16], [13, 17, 19]]
+mm.__str__(mm1)
+mm.show_matrix(mm1)
+print("\n")
+mm.__add__(mm1, mm2)
+
 
 
 
