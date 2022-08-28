99a4abfc211a0440720370088c88f964baef9b81
mrgray <mister_gray@list.ru>
2022-08-28 15:51:44 +0300

    lesson 7 task 3 prom set

---
+++ lesson-7/task_3.py
@@ -0,0 +1,11 @@
+class Cell:
+    number_of_cells = 0
+
+    def __add__(self, other):
+        pass
+    def __sub__(self, other):
+        pass
+    def __mul__(self, other):
+        pass
+    def __truediv__(self, other):
+        pass
